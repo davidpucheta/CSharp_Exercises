0f3da2b8ce2c82a8f6f107bc3b4fdc0b82ebd3b4
David Reyes-Pucheta <davidpucheta@gmail.com>
2019-12-19 12:35:22 -0600

    Add project files.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.ElapsedTime.TotalMilliseconds);
+
+            stopWatch.Start();
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.ElapsedTime.TotalMilliseconds);
+
+            stopWatch.Start();
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.ElapsedTime.TotalMilliseconds);
+
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Stopwatch
+    {
+        private bool _isRunning;
+        private DateTime _startTime;
+
+        public TimeSpan ElapsedTime { get; private set; }
+
+        public Stopwatch()
+        {
+            _startTime = DateTime.MinValue;
+            _isRunning = false;
+        }
+
+        public void Start()
+        {
+            if (_isRunning == false)
+            {
+                _startTime = DateTime.Now;
+                _isRunning = true;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stopwatch already running");
+            }
+        }
+
+        public void Stop()
+        {
+            ElapsedTime = ElapsedTime.Add(DateTime.Now - _startTime);
+            _isRunning = false;
+        }
+
+    }
+
+}
+
