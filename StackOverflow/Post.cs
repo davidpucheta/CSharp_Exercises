6e228f8f252da33b2dc3c7463dbab65f4a5831ad
David Reyes-Pucheta <davidpucheta@gmail.com>
2019-12-20 13:16:31 -0600

    Exercise 2 final

--- StackOverflow/Post.cs
+++ StackOverflow/Post.cs
@@ -7,7 +7,7 @@ namespace StackOverflow
     class Post
     {
         private int _votes;
-        private DateTime _creationDateTime { get; set; }
+        private DateTime _creationDateTime;
         public string Title { get; set; }
         public string Description { get; set; }
         
