bb6d6ad4245aee59017229c7e667313ff12d69d5
David Reyes-Pucheta <davidpucheta@gmail.com>
2019-12-20 12:05:07 -0600

    Exercise 2: Design a Post class

---
+++ StackOverflow/Post.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackOverflow
+{
+    class Post
+    {
+        private int _votes;
+        private DateTime _creationDateTime { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        
+
+        public Post()
+        {
+            _votes = 0;
+            _creationDateTime = DateTime.Now;
+        }
+
+        public void UpVote()
+        {
+            _votes = _votes + 1;
+
+        }
+
+        public void DownVote()
+        {
+            _votes = _votes - 1;
+        }
+
+        public int CurrentVotes()
+        {
+            return _votes;
+        }
+
+    }
+}
---
+++ StackOverflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StackOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var post = new Post();
+
+            post.Title = "Post title";
+            post.Description = "Body of post...";
+
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+
+            post.DownVote();
+            post.DownVote();
+
+            Console.WriteLine($"Title: {post.Title}");
+            Console.WriteLine($"Description: {post.Description}");
+            Console.WriteLine($"Votes: {post.CurrentVotes()}");
+
+
+        }
+    }
+}
